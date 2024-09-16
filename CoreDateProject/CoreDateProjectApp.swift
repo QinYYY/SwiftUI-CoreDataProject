150304fabfa0fb0971b1b9a1941b34dbfb3bb1b4
QinYYY <qinyang_c@outlook.com>
2024-09-16 16:10:10 +0930

    first c

---
+++ CoreDateProject/CoreDateProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoreDateProjectApp.swift
+//  CoreDateProject
+//
+//  Created by QinY on 16/9/2024.
+//
+
+import SwiftUI
+
+@main
+struct CoreDateProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
